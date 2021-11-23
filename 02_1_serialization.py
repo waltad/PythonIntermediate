13b2c80cbd97bb99c07c66b74aa79064cc75ae4a
Tadek <tadek.waluga@gmail.com>
2021-11-23 19:39:29 +0100

    Changing file 02_1_serialization.py

--- 02_1_serialization.py
+++ 02_1_serialization.py
@@ -27,3 +27,16 @@ Masz plik tekstowy z zawartością:
 Napisz program, który wypisze średnią z liczb z pola "right_side", średnią z pola "left_side" oraz średnią z obu pól.
 Podpowiedź
 Użyj funkcji open, żeby otworzyć plik, a następnie funkcji load, żeby skonwertować dane JSON na format Pythona."""
+from json import load
+
+
+def get_avg(*args):
+    return sum(args) / len(args)
+
+
+if __name__ == '__main__':
+    with open('data.txt', 'r') as f:
+        data = load(f)
+        print('Right side:', get_avg(*data['right_side']))
+        print('Left side:', get_avg(*data['left_side']))
+        print('All:', get_avg(*data['right_side'], *data['left_side']))
