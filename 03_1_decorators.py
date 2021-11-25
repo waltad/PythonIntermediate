2f3167a361c8e37d4d450329f2b013269cf68429
GI10 <tadus@op.pl>
2021-11-25 15:48:36 +0100

    Changing file 03_1_decorators.py

--- 03_1_decorators.py
+++ 03_1_decorators.py
@@ -1,7 +1,9 @@
-"""Napisz dekorator, który pozwoli wykonać kod dekorowanej funkcji tylko w dzień (między 7 a 22).
+"""1. Napisz dekorator, który pozwoli wykonać kod dekorowanej funkcji tylko w dzień (między 7 a 22).
 Do określenia aktualnej godziny użyj moduły datetime.
-1a Napisz podobny dekorator, jak wyżej, ale spraw by mógł on przyjmować w argumentach godziny, w których funkcja jest
-niedostępna."""
+1a. Napisz podobny dekorator, jak wyżej, ale spraw by mógł on przyjmować w argumentach godziny, w których funkcja jest
+niedostępna.
+2. Napisz dekorator, który pokaże godzinę, minutę i sekundę przed wywołaniem kodu dekorowanej funkcji oraz po wywołaniu
+kodu dekorowanej funkcji."""
 
 from datetime import datetime
 
@@ -22,6 +24,15 @@ def check_time_set(from_=7, to_=22):
      return dec
 
 
+def before_after_time(func):
+    def wrapper():
+        print(datetime.now().strftime('%H:%M:%S'))
+        result = func()
+        print(datetime.now().strftime('%H:%M:%S'))
+        return result
+    return wrapper
+
+
 @check_time
 def say_something():
     print('Hello!')
@@ -29,10 +40,15 @@ def say_something():
 
 @check_time_set(15, 20)
 def say_something_set():
-    print('The time is set correctly')
+    print('The time is set correctly.')
+
+
+@before_after_time
+def say_something_meanwhile():
+    print('Have a good time!')
 
 
 if __name__ == '__main__':
     say_something()
-
     say_something_set()
+    say_something_meanwhile()
