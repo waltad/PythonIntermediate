dcdef70eee7ef3ebad4a6ab8d5c4a92b45d9b80f
GI10 <tadus@op.pl>
2021-11-25 11:33:49 +0100

    Changing file 02_2_serialization.py

--- 02_2_serialization.py
+++ 02_2_serialization.py
@@ -70,3 +70,36 @@ Wynikowe dane w JSON:
 ]
 Zauważ, że dane wynikowe są sformatowane tak, że pierwsza linijka CSV to nazwy pól. Zauważ, że wynikowy JSON liczby ma
 zapisane we właściwym typie."""
+# import json
+from json import dumps, dump
+
+
+def convert_to_json(csv):
+    data_per_line = csv.split('\n')
+    fields = data_per_line[0].split(',')
+    data = data_per_line[1:]
+
+    result_json = []
+
+    def _ensure_proper_type(value: str):
+        return int(value) if value.isdigit() else value
+
+    for row in data:
+        sub_result = {}
+        cells = row.split(',')
+        for cell, field in zip(cells, fields):
+            sub_result[field.strip()] = _ensure_proper_type(cell.strip())
+        result_json.append(sub_result)
+
+    return dumps(result_json, indent=4)
+
+
+if __name__ == '__main__':
+    with open('data.csv', 'r') as f:
+        csv_data = ''
+        for line in f:
+            csv_data += line
+    with open('data.json', 'w') as out_file:
+        json_data = convert_to_json(csv_data)
+        dump(json_data, out_file, indent=2)
+        print(json_data)
